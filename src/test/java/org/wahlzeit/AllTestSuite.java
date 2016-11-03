1bbf12e134b18c83e79aa1d0bd36fcc84ddeec35
Daniel Quick <daniel.quick@gmx.net>
2016-11-03 18:30:09 +0100

    added testsuite AllTestSuite.java which instantiates every Testcase in wahlzeit

---
+++ src/test/java/org/wahlzeit/AllTestSuite.java
@@ -0,0 +1,54 @@
+package org.wahlzeit;
+
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.AllTests;
+import org.wahlzeit.model.*;
+import org.wahlzeit.model.persistence.AbstractAdapterTest;
+import org.wahlzeit.model.persistence.DatastoreAdapterTest;
+import org.wahlzeit.model.persistence.GcsAdapterTest;
+import org.wahlzeit.services.EmailAddressTest;
+import org.wahlzeit.services.LogBuilderTest;
+import org.wahlzeit.services.mailing.EmailServiceTest;
+import org.wahlzeit.testEnvironmentProvider.*;
+import org.wahlzeit.utils.StringUtilTest;
+import org.wahlzeit.utils.VersionTest;
+
+@RunWith(AllTests.class)
+public final class AllTestSuite {
+
+
+    public static TestSuite suite() {
+
+        TestSuite suite = new TestSuite();
+
+        suite.addTest(new JUnit4TestAdapter(AbstractAdapterTest.class));
+        suite.addTest(new JUnit4TestAdapter(DatastoreAdapterTest.class));
+        suite.addTest(new JUnit4TestAdapter(GcsAdapterTest.class));
+        suite.addTest(new JUnit4TestAdapter(AccessRightsTest.class));
+        suite.addTest(new JUnit4TestAdapter(CoordinateTest.class));
+        suite.addTest(new JUnit4TestAdapter(FlagReasonTest.class));
+        suite.addTest(new JUnit4TestAdapter(GenderTest.class));
+        suite.addTest(new JUnit4TestAdapter(LocationTest.class));
+        suite.addTest(new JUnit4TestAdapter(PhotoFilterTest.class));
+        suite.addTest(new JUnit4TestAdapter(TagsTest.class));
+        suite.addTest(new JUnit4TestAdapter(UserStatusTest.class));
+        suite.addTest(new JUnit4TestAdapter(ValueTest.class));
+        suite.addTest(new JUnit4TestAdapter(EmailServiceTest.class));
+        suite.addTest(new JUnit4TestAdapter(EmailAddressTest.class));
+        suite.addTest(new JUnit4TestAdapter(LogBuilderTest.class));
+        suite.addTest(new JUnit4TestAdapter(LocalDatastoreServiceTestConfigProvider.class));
+        suite.addTest(new JUnit4TestAdapter(RegisteredOfyEnvironmentProvider.class));
+        suite.addTest(new JUnit4TestAdapter(SysConfigProvider.class));
+        suite.addTest(new JUnit4TestAdapter(UserServiceProvider.class));
+        suite.addTest(new JUnit4TestAdapter(UserServiceProvider.class));
+        suite.addTest(new JUnit4TestAdapter(WebFormHandlerProvider.class));
+        suite.addTest(new JUnit4TestAdapter(StringUtilTest.class));
+        suite.addTest(new JUnit4TestAdapter(VersionTest.class));
+
+        return suite;
+    }
+
+
+}
